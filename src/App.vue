9fd2f8bcad3e1ce6c1c25abb42e3f4513703f107
Ezequiel Daniel Di Trana <saint.eze@gmail.com>
2019-10-02 20:43:05 -0300

    Inicio del proyecto Chuck

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <Inicio msg="BIENVENIDO A TU MUERTE"/>
+  </div>
+</template>
+
+<script>
+import Inicio from './components/Inicio.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Inicio
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
