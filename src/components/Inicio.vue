78810aea965d6a8488bbec88069b43650cb78b8e
Ezequiel Daniel Di Trana <saint.eze@gmail.com>
2019-10-02 22:46:20 -0300

    vista ok

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -1,6 +1,38 @@
 <template>
   <div class="hello; col-sm-3 offset-6">
-    <h1>{{ msg }}</h1>
+    <h1>BIENVENIDO A TU MUERTE</h1>
+    <br>
+    <br>
+    <br>
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td colspan="2">Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </table>
     <router-link to="/about">about</router-link>
     <router-view></router-view>
   </div>
@@ -8,11 +40,11 @@
 
 <script>
 export default {
-  name: 'Inicio',
+  name: "Inicio",
   props: {
     msg: String
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
