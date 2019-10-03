b39af06b1375ae48d93218ce2251988c31e9bb2a
Ezequiel Daniel Di Trana <saint.eze@gmail.com>
2019-10-02 21:28:36 -0300

    puto router

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -1,17 +1,51 @@
 <template>
-  <div class="hello; col-sm-3 offset-6">
+  <div class="hello">
     <h1>{{ msg }}</h1>
-    
+
+    <br>
+    <br>
+    <br>
+    <table class="table table-hover; col-sm-3 offset-8">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>Martin</td>
+          <td>Marina</td>
+          <td>Ezequiel</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Otto</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td colspan="2">Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Inicio',
+  name: "Inicio",
   props: {
     msg: String
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
