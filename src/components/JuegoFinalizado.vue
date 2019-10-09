01f00acbe423f77cde919631432e71878f8aa29e
Marina Burkhardt <marina.burkhardt@globant.com>
2019-10-08 23:40:19 -0300

    Adding API call

--- src/components/JuegoFinalizado.vue
+++ src/components/JuegoFinalizado.vue
@@ -1,27 +1,85 @@
 <template>
-  <div id="mainDiv">
-    <img src="../assets/background/thumbUp.png" alt="bckgound" class="bg" />
-    <h1>Saraza</h1>
+  <div>
+    <div class="speech-bubble">
+      {{currentJoke.joke}}
+    </div>
+
+    <!-- solo para debuggear es el boton! borrar luego -->
+    <button @click="fetchRandomJoke()">
+        Queres un chiste random? clickeame
+    </button>
+    <img src="../assets/background/finalizadoJoke.png" alt="backgound" class="bg" />
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
+const API_RANDOM_JOKE_ENDPOINT = 'https://api.icndb.com/jokes/random'
+
 export default {
-  name: "JuegoFinalizado"
-};
+    name: 'JuegoFinalizado',
+
+    mounted() {
+      this.fetchRandomJoke()
+    },
+
+    data: function() {
+      return {
+        currentJoke: null
+      }
+    },
+
+    methods: {
+      fetchRandomJoke() {
+        axios.get(API_RANDOM_JOKE_ENDPOINT)
+          .then(({ data }) => this.currentJoke = data.value)
+      }
+    }
+}
 </script>
 
-<style>
-#mainDiv {
-  height: 100%;
-}
+<style scoped>
+
 img.bg {
-    min-height: 30%;
-    width: 30%;
-    height: auto;
-    position: relative;
-    top: 0;
-    left: 0;
+  min-height: 30%;
+  max-width: 30%;
+  height: auto;
+  position: relative;
+  top: 0;
+  left: 10%;
+  float: left ;
+}
+
+button {
+  background-color: rgb(59, 186, 0);
+  border-radius: 8px;
+  float: inline-end;
+}
+
+.speech-bubble {
+  position: relative;
+  background: #ca7a12;
+  border-radius: .4em;
+  padding: 15px;
+  text-align: center;
+  max-width: 60%;
+  margin-left: auto;
+  margin-right: auto;
+}
+.speech-bubble:after {
+  content: '';
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  width: auto;
+  height: auto;
+  border: 51px solid transparent;
+  border-top-color: #ca7a12;
+  border-bottom: 0;
+  border-left: 0;
+  margin-left: -25.5px;
+  margin-bottom: -51px;
 }
 
 </style>
