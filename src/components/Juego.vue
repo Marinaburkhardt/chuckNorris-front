4897573365a6c07d1ed1d435ff54e2d936b9d4f2
Marina Burkhardt <marina.burkhardt@globant.com>
2019-10-17 20:43:56 -0300

    Fixing div issue

--- src/components/Juego.vue
+++ src/components/Juego.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
         <br>
-        <div class="row justify-content-around" style="width: 100%;overflow-x: scroll(or auto);">
+        <div class="row" style="width: 100%;overflow-x: scroll(or auto);">
        
             <!-- Estado de partida -->
-            <div class="col-sm-1">
+            <div class="col-1 container-fluid">
                 <h5>Resultados partidas</h5>
                 <table>
                 <thead>
@@ -23,28 +23,26 @@
             </div>
 
             <!-- Parte principal -->
-            <div class="col-sm-1">
+            <div class="col-1 container-fluid" >
                 <h5>A jugar!</h5>
                 <div class="example example-cards">  
                     <div id="cardsA" class="cards cards--fan  cards--overlap">
-                    
-                    <div class="card">
-                    <div class="flipper">
-                        <div class="face">1</div>
-                    </div>
-                    </div>
-                    
-                    <div class="card">
-                    <div class="flipper">
-                        <div class="face">2</div>
-                    </div>
-                    </div>
-                    </div>
-                    
-                    </div>
+                        <div class="card">
+                        <div class="flipper">
+                            <div class="face">1</div>
+                        </div>
+                        </div>
+                        
+                        <div class="card">
+                        <div class="flipper">
+                            <div class="face">2</div>
+                        </div>
+                        </div>
+                    </div>    
+                </div>
 
-                    <br><br><br>
-                    <div id="cardsB" class="cards cards--reverse cards--fan cards--overlap">
+                <br><br><br>
+                <div id="cardsB" class="cards cards--reverse cards--fan cards--overlap">
                     
                     <div class="card">
                     <div class="flipper">
@@ -63,12 +61,11 @@
                     
                     </div>
                     
-                    </div>   
-                </div>
+                </div>   
             </div>
 
             <!-- Instrucciones -->
-            <div class="col-sm-2">
+            <div class="col-1 container-fluid">
                 <h5>Instrucciones</h5>
                 <br>
                 <p>Los valores de las figuras tienen valores equivalentes al juego "piedra, papel o tijer"</p>
