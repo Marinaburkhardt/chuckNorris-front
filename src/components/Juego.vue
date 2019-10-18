5fab541b1e3fcb1e288f0c3a056aba3beb39049d
Marina Burkhardt <marina.burkhardt@globant.com>
2019-10-17 22:33:28 -0300

    Adding figures/cards

--- src/components/Juego.vue
+++ src/components/Juego.vue
@@ -1,10 +1,10 @@
 <template>
-    <div>
+    <div class="container-fluid">
         <br>
         <div class="row" style="width: 100%;overflow-x: scroll(or auto);">
        
             <!-- Estado de partida -->
-            <div class="col-1 container-fluid">
+            <div class="col-2">
                 <h5>Resultados partidas</h5>
                 <table>
                 <thead>
@@ -23,40 +23,42 @@
             </div>
 
             <!-- Parte principal -->
-            <div class="col-1 container-fluid" >
+            <div class="col-8 d-flex flex-column" >
                 <h5>A jugar!</h5>
                 <div class="example example-cards">  
-                    <div id="cardsA" class="cards cards--fan  cards--overlap">
-                        <div class="card">
+                    <div id="cardsA" class="cards cards--fan  cards--overlap ">
+                        <div class="card col-6">
                         <div class="flipper">
-                            <div class="face">1</div>
+                            <img class="face" src="../assets/background/finalizadoPerdedor.png"/>
                         </div>
                         </div>
                         
-                        <div class="card">
+                        <div class="card col-6">
                         <div class="flipper">
-                            <div class="face">2</div>
+                            <img class="face" src="../assets/background/finalizadoJoke.png" />
                         </div>
                         </div>
                     </div>    
                 </div>
 
-                <br><br><br>
+                <br>
+                <h5>Elije tu figura!</h5>
+                <br>
                 <div id="cardsB" class="cards cards--reverse cards--fan cards--overlap">
                     
-                    <div class="card">
+                    <div class="card col-4">
                     <div class="flipper">
-                        <div class="face">1</div>
+                        <img class="face" src="../assets/background/finalizadoJoke.png" />
                     </div>
                     </div>
-                    <div class="card">
+                    <div class="card col-4">
                     <div class="flipper">
-                        <div class="face">2</div>
+                        <img class="face" src="../assets/background/finalizadoPerdedor.png"/>
                     </div>
                     </div>
-                    <div class="card">
+                    <div class="card col-4">
                     <div class="flipper">
-                        <div class="face">3</div>
+                        <img class="face" src="../assets/chuck-Inicio2.png"/>
                     </div>
                     
                     </div>
@@ -65,8 +67,8 @@
             </div>
 
             <!-- Instrucciones -->
-            <div class="col-1 container-fluid">
-                <h5>Instrucciones</h5>
+            <div class="col-2">
+                <h5><u>Instrucciones</u></h5>
                 <br>
                 <p>Los valores de las figuras tienen valores equivalentes al juego "piedra, papel o tijer"</p>
 
@@ -120,7 +122,71 @@ export default {
 
 <style scoped>
 
+.example{
+  padding: 2em 0;
+}
+.example[onclick]{
+  cursor: pointer;
+}
 
+.cards{
+  display: inline-flex;
+  border: .25em solid rgba(0,0,0,0.25);
+  border-radius: .35em;
+  padding: 1em 2em;
+  min-width: 3em;
+  min-height: 7em;
+}
 
+#cardsA{
+  font-size: 1.5em;
+}
+
+.card{
+  cursor: pointer;
+  color: #333;
+  font-size: 4em;
+}
+.example .card-group{
+  padding: 2em 0;
+}
+
+.card{
+  transform: translateX(0) translateY(0) rotateZ(0);
+}
+
+.card.absolute{
+  position: absolute;
+  top:0; left:0;
+  transition: none;
+}
+
+.card.hidden{
+  visibility: hidden;
+}
+
+.card .face,
+.card .back{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: 800;
+  max-width:40%;
+}
+
+.card:hover .face,
+.card:hover .back{
+  color: hotpink;
+}
+
+.card:hover .face:after,
+.card:hover .back:after{
+  display: block;
+  position: absolute;
+  top: 0; left: 0; right:0; bottom:0;
+  border: 0.07em solid;
+  border-radius: 0.08em;
+  content: '';
+}
 
 </style>
