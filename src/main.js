fcdf61ddd8fd4efabed5f065a7751cb1aa7f1ac6
Ezequiel Daniel Di Trana <saint.eze@gmail.com>
2019-11-06 20:45:00 -0300

    alertService

--- src/main.js
+++ src/main.js
@@ -6,15 +6,18 @@ import store from './store/store';
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Alerts from "./services/Alerts";
 
 // Vue.use(VueRouter)
 Vue.use(BootstrapVue)
 Vue.use(Vuex)
+// Vue.use(Alerts)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  Alerts,
   render: h => h(App),
 }).$mount('#app');
