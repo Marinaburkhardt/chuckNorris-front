f8bb6f2d82650262ef46c7d338e0c37e7b1c8ec8
Ezequiel Daniel Di Trana <saint.eze@gmail.com>
2019-11-06 22:38:05 -0300

    imagenes aleatorias

--- src/components/Juego.vue
+++ src/components/Juego.vue
@@ -1,114 +1,109 @@
 <template>
-    <div class="container-fluid">
-        <br>
-        <div class="row" style="width: 100%;overflow-x: scroll(or auto);">
-       
-            <!-- Estado de partida -->
-            <div class="col-2">
-                <h5>Resultados partidas</h5>
-                <table>
-                <thead>
-                    <tr>
-                    <th style="text-align: center" >N° turno</th>
-                    <th style="text-align: center">Ganador</th>
-                    </tr>
-                </thead>
-                <tbody v-for="jugador in ganadoresTurno" :key="jugador">
-                    <tr>
-                    <th style="text-align: center">{{jugador.turno}}</th>
-                    <td style="text-align: center">{{jugador.nick}}</td>
-                    </tr>
-                </tbody>
-                </table>
-            </div>
+  <div class="container-fluid">
+    <br />
+    <div class="row" style="width: 100%;overflow-x: scroll(or auto);">
+      <!-- Estado de partida -->
+      <div class="col-2">
+        <h5>Resultados partidas</h5>
+        <table>
+          <thead>
+            <tr>
+              <th style="text-align: center">N° turno</th>
+              <th style="text-align: center">Ganador</th>
+            </tr>
+          </thead>
+          <tbody v-for="jugador in ganadoresTurno" :key="jugador">
+            <tr>
+              <th style="text-align: center">{{jugador.turno}}</th>
+              <td style="text-align: center">{{jugador.nick}}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
 
-            <!-- Parte principal -->
-            <div class="col-8 d-flex flex-column" >
-                <h5>A jugar!</h5>
+      <!-- Parte principal -->
+      <div class="col-8 d-flex flex-column">
+        <h5>A jugar!</h5>
 
-                <div class="row justify-content-around">
-                  <div class="col-4">
-                      <vue-flipcard>
-                        <template slot="front" @flip="onFlip">
-                          <img class="imgJugada" src="../assets/background/finalizadoPerdedor.png"/>
-                        </template>
-                        <template slot="back" v-if="figuraLanzada">
-                          <img class="imgJugada" src="../assets/background/finalizadoJoke.png" />
-                        </template>
-                      </vue-flipcard>
-                  </div>
+        <div class="row justify-content-around">
+          <div class="col-4">
+            <vue-flipcard>
+              <template slot="front" @flip="onFlip">
+                <img class="imgJugada" src="../assets/background/finalizadoPerdedor.png" />
+              </template>
+              <template slot="back" v-if="figuraLanzada">
+                <img class="imgJugada" src="../assets/background/finalizadoJoke.png" />
+              </template>
+            </vue-flipcard>
+          </div>
 
-                  <div class="col-4">
-                      <vue-flipcard>
-                        <template slot="front" @flip="onFlip">
-                          <img class="imgJugada" src="../assets/background/finalizadoPerdedor.png"/>
-                        </template>
-                        <template slot="back">
-                          <img class="imgJugada" src="../assets/background/finalizadoJoke.png" />
-                        </template>
-                      </vue-flipcard>
-                  </div>
-
-                </div>
-                
-
-                <br>
-                <h5>Elije tu figura!</h5>
-                <br>
-                <div id="cardsB" class="cards cards--reverse cards--fan cards--overlap">
-                    
-                    <div class="card col-4">
-                    <div class="flipper">
-                        <img class="face" v-bind:src="randomPickerFigura(figurasSombrero)"/>
-                    </div>
-                    </div>
-                    <div class="card col-4">
-                    <div class="flipper">
-                        <img class="face" :src="randomPickerFigura(figurasSonrisa)"/>
-                    </div>
-                    </div>
-                    <div class="card col-4">
-                    <div class="flipper">
-                        <img class="face" :src="randomPickerFigura(figurasArmas)"/>
-                    </div>
-                    </div>
-                    
-                </div>   
-            </div>
-
-            <!-- Instrucciones -->
-            <div class="col-2">
-                <h5><u>Instrucciones</u></h5>
-                <br>
-                <p>Los valores de las figuras tienen valores equivalentes al juego "piedra, papel o tijer"</p>
-
-                <table>
-                <thead>
-                    <tr>
-                    <th style="text-align: center" scope="col">Figura</th>
-                    <th style="text-align: center" scope="col">Equivalente</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    <th style="text-align: center">Arma</th>
-                    <td style="text-align: center">Piedra</td>
-                    </tr>
-                    <tr>
-                    <th style="text-align: center">Sonrisa</th>
-                    <td style="text-align: center">Papel</td>
-                    </tr>
-                    <tr>
-                    <th style="text-align: center">Sombrero</th>
-                    <td style="text-align: center">Tijera</td>
-                    </tr>
-                </tbody>
-                </table>
-            </div>
-
+          <div class="col-4">
+            <vue-flipcard>
+              <template slot="front" @flip="onFlip">
+                <img class="imgJugada" src="../assets/background/finalizadoPerdedor.png" />
+              </template>
+              <template slot="back">
+                <img class="imgJugada" src="../assets/background/finalizadoJoke.png" />
+              </template>
+            </vue-flipcard>
+          </div>
         </div>
-    </div>
 
+        <br />
+        <h5>Elije tu figura!</h5>
+        <br />
+        <div id="cardsB" class="cards cards--reverse cards--fan cards--overlap">
+          <div class="card col-4">
+            <div class="flipper">
+              <img :src="require(`@/assets/gestos-jugadas/sombrero/sombrero_${numeroRandom}`)" alt="">
+            </div>
+          </div>
+          <div class="card col-4">
+            <div class="flipper">
+              <img :src="require(`@/assets/gestos-jugadas/gestos/gestos_${numeroRandom}`)" alt="">
+            </div>
+          </div>
+          <div class="card col-4">
+            <div class="flipper">
+              <img :src="require(`@/assets/gestos-jugadas/armas/armas_${numeroRandom}`)" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Instrucciones -->
+      <div class="col-2">
+        <h5>
+          <u>Instrucciones</u>
+        </h5>
+        <br />
+        <p>Los valores de las figuras tienen valores equivalentes al juego "piedra, papel o tijer"</p>
+
+        <table>
+          <thead>
+            <tr>
+              <th style="text-align: center" scope="col">Figura</th>
+              <th style="text-align: center" scope="col">Equivalente</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th style="text-align: center">Arma</th>
+              <td style="text-align: center">Piedra</td>
+            </tr>
+            <tr>
+              <th style="text-align: center">Sonrisa</th>
+              <td style="text-align: center">Papel</td>
+            </tr>
+            <tr>
+              <th style="text-align: center">Sombrero</th>
+              <td style="text-align: center">Tijera</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -127,17 +122,13 @@ export default {
       ],
       figuraLanzada : true,
 
-      mostrarCartas: false,
-      piedra: '',
-      papel: '',
-      tijera: '',
+      numeroRandom: '',
       
       figurasSombrero: [
         "../assets/gestos-jugadas/sombrero/sombrero_01.png",
         "../assets/gestos-jugadas/sombrero/sombrero_02.png",
         "../assets/gestos-jugadas/sombrero/sombrero_03.png",
         "../assets/gestos-jugadas/sombrero/sombrero_04.png",
-        "../assets/gestos-jugadas/sombrero/sombrero_05.png"
       ],
       figurasSonrisa: [
         "../assets/gestos-jugadas/gestos/gestos_01.png",
@@ -153,6 +144,10 @@ export default {
       ],
       figuraElegida: ''
     };
+    
+  },
+   created() {
+    this.randomPickerFigura();
   },
   methods: {
 
@@ -160,22 +155,20 @@ export default {
       this.$refs.flipcard.flip()
     },
 
-    randomPickerFigura (arrayFigura){
-      let numeroRandom = Math.floor(Math.random() * arrayFigura.length)
-      return arrayFigura[numeroRandom]
-      //return "../assets/gestos-jugadas/gestos/gestos_01.png"
+    randomPickerFigura (){
+      this.numeroRandom = (Math.floor(Math.random() * 4)+1)
+      this.numeroRandom = '0' + this.numeroRandom + '.png';
+      
     }
-
-    }
+  }
 }
 </script>
 
 <style scoped>
-
 .FlipCard {
   direction: vertical;
-  width:50px;
-  height:50px;
+  width: 50px;
+  height: 50px;
 }
 
 .imgJugada {
@@ -185,67 +178,70 @@ export default {
   display: inline-flex;
 }
 
-.example{
+.example {
   padding: 2em 0;
 }
-.example[onclick]{
+.example[onclick] {
   cursor: pointer;
 }
 
-.cards{
+.cards {
   display: inline-flex;
-  border: .25em solid rgba(0,0,0,0.25);
-  border-radius: .35em;
+  border: 0.25em solid rgba(0, 0, 0, 0.25);
+  border-radius: 0.35em;
   padding: 1em 2em;
   min-width: 3em;
   min-height: 7em;
 }
 
-#cardsA{
+#cardsA {
   font-size: 1.5em;
 }
 
-.card{
+.card {
   cursor: pointer;
   color: #333;
   font-size: 4em;
 }
-.example .card-group{
+.example .card-group {
   padding: 2em 0;
 }
 
-.card.absolute{
+.card.absolute {
   position: absolute;
-  top:0; left:0;
+  top: 0;
+  left: 0;
   transition: none;
 }
 
-.card.hidden{
+.card.hidden {
   visibility: hidden;
 }
 
 .card .face,
-.card .back{
+.card .back {
   display: flex;
   align-items: center;
   justify-content: center;
   font-weight: 800;
-  max-width:40%;
+  max-width: 40%;
 }
 
 .card:hover .face,
-.card:hover .back{
+.card:hover .back {
   color: hotpink;
 }
 
 .card:hover .face:after,
-.card:hover .back:after{
+.card:hover .back:after {
   display: block;
   position: absolute;
-  top: 0; left: 0; right:0; bottom:0;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
   border: 0.07em solid;
   border-radius: 0.08em;
-  content: '';
+  content: "";
 }
-
 </style>
