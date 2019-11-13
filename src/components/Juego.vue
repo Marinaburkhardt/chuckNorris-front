fc9d68841f50d1c84df04e57db3a4fa2b25e2afa
Marina Burkhardt <marina.burkhardt@globant.com>
2019-11-13 20:04:41 -0300

    Resize cards played

--- src/components/Juego.vue
+++ src/components/Juego.vue
@@ -52,19 +52,19 @@
         <br />
         <h5>Elije tu figura!</h5>
         <br />
-        <div id="cardsB" class="cards cards--reverse cards--fan cards--overlap">
+        <div class="cards">
           <div class="card col-4">
-            <div class="flipper">
+            <div class="imgJugada">
               <img :src="require(`@/assets/gestos-jugadas/sombrero/sombrero_${numeroRandom}`)" alt="">
             </div>
           </div>
           <div class="card col-4">
-            <div class="flipper">
+            <div class="imgJugada">
               <img :src="require(`@/assets/gestos-jugadas/gestos/gestos_${numeroRandom}`)" alt="">
             </div>
           </div>
           <div class="card col-4">
-            <div class="flipper">
+            <div class="imgJugada">
               <img :src="require(`@/assets/gestos-jugadas/armas/armas_${numeroRandom}`)" alt="">
             </div>
           </div>
@@ -165,24 +165,12 @@ export default {
 </script>
 
 <style scoped>
-.FlipCard {
-  direction: vertical;
-  width: 50px;
-  height: 50px;
-}
 
 .imgJugada {
   position: relative;
-  min-width: 3em;
-  min-height: 7em;
+  max-width:100%;
+  max-height:100%;
   display: inline-flex;
-}
-
-.example {
-  padding: 2em 0;
-}
-.example[onclick] {
-  cursor: pointer;
 }
 
 .cards {
@@ -190,12 +178,6 @@ export default {
   border: 0.25em solid rgba(0, 0, 0, 0.25);
   border-radius: 0.35em;
   padding: 1em 2em;
-  min-width: 3em;
-  min-height: 7em;
-}
-
-#cardsA {
-  font-size: 1.5em;
 }
 
 .card {
@@ -203,12 +185,9 @@ export default {
   color: #333;
   font-size: 4em;
 }
-.example .card-group {
-  padding: 2em 0;
-}
 
 .card.absolute {
-  position: absolute;
+  position: relative;
   top: 0;
   left: 0;
   transition: none;
@@ -220,11 +199,9 @@ export default {
 
 .card .face,
 .card .back {
-  display: flex;
   align-items: center;
   justify-content: center;
-  font-weight: 800;
-  max-width: 40%;
+  /* font-weight: 800; */
 }
 
 .card:hover .face,
@@ -234,14 +211,12 @@ export default {
 
 .card:hover .face:after,
 .card:hover .back:after {
-  display: block;
-  position: absolute;
-  top: 0;
+  position: relative;
+  /* top: 0;
   left: 0;
   right: 0;
-  bottom: 0;
+  bottom: 0; */
   border: 0.07em solid;
   border-radius: 0.08em;
-  content: "";
 }
 </style>
