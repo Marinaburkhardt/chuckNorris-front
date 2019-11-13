37ce3917c1b817975bc63e79215ac7ad7301eee5
Ezequiel Daniel Di Trana <saint.eze@gmail.com>
2019-11-13 20:55:15 -0300

    login ok

--- src/services/RestServices.js
+++ src/services/RestServices.js
@@ -3,6 +3,11 @@ import axios from 'axios'
 export default {
     getChisteRandom() {
         return axios.get('https://api.icndb.com/jokes/random')
+    },
+
+    login(json) {
+        console.log('json:', json)
+        return axios.post('http://localhost:3000/api/jugador/login',json)
     }
 }
     
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,8 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+// import {restServices} from '../services/RestServices.js'
+
+const restServices = require('../services/RestServices.js')
 
 Vue.use(Vuex);
 
@@ -9,10 +12,26 @@ export default new Vuex.Store({
     },
     mutations: {
         setIsAuthenticated(state, jsonLogin) {
+            console.log('jsonLogin: ', jsonLogin);
+            console.log('state: ', state);
+            restServices.default.login({
+                "nick": "edditrana",
+                "password": "admin"
+            }).then(function (response) {
+                console.log('response: ', response);
+                if (response.status == 200) {
+                    state.isAuthenticated = true;
+                } else {
+                    
+                }
+            })
+                .catch(function (error) {
+                    console.log(error);
+                });
+
             // TODO: pegada al back para validar si existe o no el jugador
-            state.isAuthenticated = true;
         }
-    },  
+    },
     getters: {
         estaLogueado: state => state.isAuthenticated
     }
